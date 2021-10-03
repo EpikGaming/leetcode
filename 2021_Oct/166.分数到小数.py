3d5e6a2dcf1871dbf557836a9ccb8745bde96533
EpikGaming <278019451@qq.com>
2021-10-03 21:04:59 +0800

    10月每日一题(3/31)

--- 2021_Oct/166.分数到小数.py
+++ 2021_Oct/166.分数到小数.py
@@ -1,7 +1,19 @@
+"""
+给定两个整数，分别表示分数的分子numerator 和分母denominator，
+以字符串形式返回小数。
+如果小数部分为循环小数，则将循环的部分括在括号内。
+如果存在多个答案，只需返回任意一个。
+对于所有给定的输入，保证答案字符串的长度小于 10^4 。
+"""
+"""
+模拟除法，对余数乘10
+"""
 class Solution:
     def fractionToDecimal(self, numerator: int, denominator: int):
         def function(a: int, b: int, tag=0):
+            #把符号去掉，方便处理
             if a * b < 0:
+                #由于python无数据类型溢出，所以可以直接相乘
                 flag = -1
             else:
                 flag = 1
@@ -19,16 +31,19 @@ class Solution:
                 n = a // b
                 if a in dic:
                     res.insert(dic[a], "(")
+                    #列表循环部分前插入"("
                     res.append(")")
                     break
                 res.append(str(n))
                 dic[a] = i + 2
+                #前两位是整数部分str(n)和小数点"."所以字典值为i+2,起始值为0+2=2
                 if m == 0:
                     break
                 i += 1
             return "-" + "".join(res) if flag == -1 else "".join(res)
         return function(numerator, denominator, 10000)
+        #由于答案字符串长度小于10^4,所以tag=10000
 
 s = Solution()
-test1 = [1, 5]
-print(s.fractionToDecimal(test1[0], test1[1]))
+print(s.fractionToDecimal(1, 5))  # ->0.2
+print(s.fractionToDecimal(4, 333))  # ->0.(012)
